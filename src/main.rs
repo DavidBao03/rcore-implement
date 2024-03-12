b11ca622b8782301ff3f8b16642ec03a7cced04a
DavidBao03 <1353406961@qq.com>
2024-03-12 21:12:14 +0800

    finish ch1 part2

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,52 @@
 #![feature(panic_info_message)]
 
 use core::arch::global_asm;
+use log::*;
 
 #[macro_use]
 mod console;
 mod lang_items;
+mod logging;
 mod sbi;
-
-
 
 global_asm!(include_str!("entry.asm"));
 
 #[no_mangle]
 pub fn rust_main() -> ! {
+    extern "C" {
+        fn stext();
+        fn etext();
+        fn sbss();
+        fn ebss();
+        fn srodata();
+        fn erodata();
+        fn sdata();
+        fn edata();
+        fn boot_stack();
+        fn boot_stack_top();
+    }
     clear_bss();
-    // console_putchar('O' as usize);
-    // console_putchar('K' as usize);
-    println!("Hello My OS!");
+    logging::init();
+    println!("[kernel] Hello, world!");
+    info!(
+        "[kernel] .text [{:#x}, {:#x})",
+        stext as usize,
+        etext as usize
+    );
+    info!(
+        "[kernel] .rodata [{:#x}, {:#x})",
+        srodata as usize, erodata as usize
+    );
+    info!(
+        "[kernel] .data [{:#x}, {:#x})",
+        sdata as usize, edata as usize
+    );
+    info!(
+        "[kernel] boot_stack top=bottom={:#x}, lower_bound={:#x}",
+        boot_stack_top as usize, boot_stack as usize
+    );
+    info!("[kernel] .bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    
     panic!("Shutdown machine!");
 }
 
